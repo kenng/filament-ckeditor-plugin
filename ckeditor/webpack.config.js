a6e341c13aca2152a0340a2a7876223219e3f547
Ken Ng <ken.ng@hostastay.com>
2023-04-04 15:56:11 +0800

    feat: add ckeditor source with live reload

---
+++ ckeditor/webpack.config.js
@@ -0,0 +1,124 @@
+/**
+ * @license Copyright (c) 2014-2023, CKSource Holding sp. z o.o. All rights reserved.
+ * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
+ */
+
+'use strict';
+
+/* eslint-env node */
+
+const path = require('path');
+const webpack = require('webpack');
+const { bundler, styles } = require('@ckeditor/ckeditor5-dev-utils');
+const {
+    CKEditorTranslationsPlugin,
+} = require('@ckeditor/ckeditor5-dev-translations');
+const TerserWebpackPlugin = require('terser-webpack-plugin');
+
+const isDevServer = process.env.WEBPACK_DEV_SERVER;
+
+module.exports = {
+    devtool: 'source-map',
+    performance: { hints: false },
+
+    entry: path.resolve(__dirname, 'src', 'ckeditor.js'),
+
+    output: {
+        // The name under which the editor will be exported.
+        library: 'ClassicEditor',
+
+        path: path.resolve(__dirname, 'build'),
+        filename: 'ckeditor.js',
+        libraryTarget: 'umd',
+        libraryExport: 'default',
+    },
+
+    devServer: {
+        client: {
+            overlay: true,
+        },
+        static: [
+            {
+                directory: path.join(__dirname, 'src'),
+                watch: true,
+                publicPath: '/',
+                serveIndex: true,
+            },
+        ],
+        compress: true,
+        open: true,
+    },
+
+    optimization: {
+        minimizer: [
+            new TerserWebpackPlugin({
+                sourceMap: true,
+                terserOptions: {
+                    output: {
+                        // Preserve CKEditor 5 license comments.
+                        comments: /^!/,
+                    },
+                },
+                extractComments: false,
+            }),
+        ],
+    },
+
+    plugins: [
+        // Disable CKEditorTranslationsPlugin when running the dev server since it clears the translations `outputDirectory`
+        // See https://github.com/ckeditor/ckeditor5/issues/700 for more information.
+        ...(isDevServer
+            ? []
+            : [
+                  new CKEditorTranslationsPlugin({
+                      // UI language. Language codes follow the https://en.wikipedia.org/wiki/ISO_639-1 format.
+                      // When changing the built-in language, remember to also change it in the editor's configuration (src/ckeditor.js).
+                      language: 'en',
+                      additionalLanguages: 'all',
+                  }),
+              ]),
+        new webpack.BannerPlugin({
+            banner: bundler.getLicenseBanner(),
+            raw: true,
+        }),
+    ],
+
+    module: {
+        rules: [
+            {
+                test: /\.svg$/,
+                use: ['raw-loader'],
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    {
+                        loader: 'style-loader',
+                        options: {
+                            injectType: 'singletonStyleTag',
+                            attributes: {
+                                'data-cke': true,
+                            },
+                        },
+                    },
+                    {
+                        loader: 'css-loader',
+                    },
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            postcssOptions: styles.getPostCssConfig({
+                                themeImporter: {
+                                    themePath: require.resolve(
+                                        '@ckeditor/ckeditor5-theme-lark',
+                                    ),
+                                },
+                                minify: true,
+                            }),
+                        },
+                    },
+                ],
+            },
+        ],
+    },
+};
